55721da568511239b832baac57309aa171265d36
egor <akhramovich.e.i@gmail.com>
2022-12-19 19:37:36 +0300

    Homework

---
+++ homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(" Первое число: ");
+            int num_A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: ");
+            int num_B = Convert.ToInt32(Console.ReadLine());
+
+            if (num_A > num_B)
+            {
+                Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+            }
